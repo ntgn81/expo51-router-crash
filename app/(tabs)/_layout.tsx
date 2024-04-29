01b2da3d53239fcff6e0fa6ff8f023470252c56e
Nam Nguyen <ntgn81@gmail.com>
2024-04-28 22:32:05 -0400

    Crash without reanimated

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Tab 1" }} />
+      <Tabs.Screen name="tab2" options={{ title: "Tab 2" }} />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/tab2.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+  return <Text>Tab 2</Text>;
+}
